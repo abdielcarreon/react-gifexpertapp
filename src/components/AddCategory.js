0d25c7061d59f6c7f08b8c10588235766b58de36
Abdiel <abdiel.carreon@outlook.com>
2022-03-07 13:28:48 -0600

    First commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const AddCategory = ({ setCategories }) => {
+
+  const [inputValue, setInputValue] = useState('');  
+  
+  const handleInputChange = ( e ) => {
+      setInputValue( e.target.value);
+  }
+
+  const handleSumbit = (e)  => {
+    e.preventDefault();
+
+    if( inputValue.trim().length > 2 ) {
+        
+        // console.log(setCategories);
+        setCategories( categor => [inputValue, ...categor ] );
+        setInputValue('');
+    }
+
+
+  }
+  return (
+    <form onSubmit={handleSumbit}>
+        <input 
+        type ="text"
+        value={ inputValue }
+        onChange={ handleInputChange }
+        />
+        
+
+    </form>
+  )
+}
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
